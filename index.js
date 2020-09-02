5095040769a6a5922ba949deb44e5f1a7dc28b9c
Yahya Faroun <yahyafaroun@gmail.com>
2020-09-02 16:18:58 +0200

    version1

---
+++ index.js
@@ -0,0 +1,30 @@
+//Verwende Express & Verweis
+const express = require('express')
+//Express Aktivieren
+const app = express()
+//EJS Aktivieren
+app.set('view engine', 'ejs')
+
+//News-Data einbinden
+const newsData = require('./newsData.json')
+
+
+//Server abhören
+app.listen(3008, () =>{
+    console.log('listening at 3008');   
+})
+
+
+//Um Public Ordner zu verwenden
+app.use(express.static('public'))
+
+
+//Routing
+app.get('/', (req, res) =>{
+    res.render('index', {newsData: newsData})
+})
+
+//404
+app.use((req, res) =>{
+    res.render('404')
+})
